2a5c55c34ef8b28bb541979f00eda204daae2973
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2025-06-16 20:14:43 +0200

    Use my scripts on startup
    no arguments launches linux_copy.sh
    -ocr with no further arguments launches linux_ocr.sh
    -ocrv with no further arguments launches linux_ocrv.sh

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,82 @@
-use std::{env, process};
+use std::{
+    env,
+    io::{self, Write},
+    path::PathBuf,
+    process::{self, Command},
+};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
     if args.len() > 0 {
-        let query: String = args.join("");
-        if let Err(e) = popup_dictionary::run(&query) {
-            eprintln!("Error: {e}");
-            process::exit(1);
+        if args.len() > 1 {
+            let mut query: String = String::new();
+            if args[0] == "-ocr" || args[0] == "-ocrv" {
+                query = args[1..].join("");
+            } else {
+                query = args.join("");
+            }
+            if let Err(e) = popup_dictionary::run(&query) {
+                eprintln!("Error: {e}");
+                process::exit(1);
+            }
+        } else {
+            if args[0] == "-ocr" {
+                let linux_copy_path: PathBuf = match dirs::config_dir() {
+                    Some(path) => path.join("popup_dictionary/linux_ocr.sh"),
+                    None => {
+                        eprintln!("No valid config path found in environment variables.");
+                        process::exit(1);
+                    }
+                };
+                let output: process::Output = Command::new("sh")
+                    .arg(linux_copy_path)
+                    .output()
+                    .expect("failed to execute linux copy");
+                println!("status: {}", output.status);
+                io::stdout().write_all(&output.stdout);
+                io::stderr().write_all(&output.stderr);
+            } else if args[0] == "-ocrv" {
+                let linux_copy_path: PathBuf = match dirs::config_dir() {
+                    Some(path) => path.join("popup_dictionary/linux_ocrv.sh"),
+                    None => {
+                        eprintln!("No valid config path found in environment variables.");
+                        process::exit(1);
+                    }
+                };
+                let output: process::Output = Command::new("sh")
+                    .arg(linux_copy_path)
+                    .output()
+                    .expect("failed to execute linux copy");
+                println!("status: {}", output.status);
+                io::stdout().write_all(&output.stdout);
+                io::stderr().write_all(&output.stderr);
+            } else {
+                let query: String = args.join("");
+                if let Err(e) = popup_dictionary::run(&query) {
+                    eprintln!("Error: {e}");
+                    process::exit(1);
+                }
+            }
         }
     } else {
-        eprintln!("Error: No text provided.");
-        process::exit(1);
+        if cfg!(target_os = "windows") {
+            //Command::new("cmd")
+        } else {
+            let linux_copy_path: PathBuf = match dirs::config_dir() {
+                Some(path) => path.join("popup_dictionary/linux_copy.sh"),
+                None => {
+                    eprintln!("No valid config path found in environment variables.");
+                    process::exit(1);
+                }
+            };
+            let output: process::Output = Command::new("sh")
+                .arg(linux_copy_path)
+                .output()
+                .expect("failed to execute linux copy");
+            println!("status: {}", output.status);
+            io::stdout().write_all(&output.stdout);
+            io::stderr().write_all(&output.stderr);
+        }
     }
 }
