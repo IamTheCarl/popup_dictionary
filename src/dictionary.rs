cc946c5eb12b08da548cb94578270a3c580e1401
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2025-06-23 16:15:11 +0200

    Include ids into DictionaryTerms

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -19,6 +19,7 @@ pub struct DictionaryEntry {
 
 #[derive(bincode::Encode, bincode::Decode, Clone, Debug)]
 pub struct DictionaryTerm {
+    pub id: String,
     pub frequency: Option<u32>,
     pub common: bool,
     pub term: String,
@@ -43,6 +44,7 @@ struct JMDict {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Word {
+    id: String,
     kanji: Vec<Kanji>,
     kana: Vec<Kana>,
     sense: Vec<Sense>,
@@ -163,6 +165,7 @@ impl Dictionary {
         let wildcard: String = String::from("*");
 
         for word in &jmdict.words {
+            let current_id: String = word.id.to_string();
             if !word.kanji.is_empty() {
                 for kanji in &word.kanji {
                     for kana in word.kana.iter().filter(|kana| {
@@ -188,6 +191,7 @@ impl Dictionary {
                         Self::insert_entry(
                             db,
                             &format!("term:{}", kanji.text),
+                            &current_id,
                             &frequency,
                             &kanji.common,
                             &kanji.text,
@@ -203,6 +207,7 @@ impl Dictionary {
                         Self::insert_entry(
                             db,
                             &format!("reading:{}", kana.text),
+                            &current_id,
                             &frequency,
                             &kana.common,
                             &kanji.text,
@@ -233,6 +238,7 @@ impl Dictionary {
                     Self::insert_entry(
                         db,
                         &format!("reading:{}", kana.text),
+                        &current_id,
                         &frequency_map.get(&kana.text),
                         &kana.common,
                         "",
@@ -258,6 +264,7 @@ impl Dictionary {
                     Self::insert_entry(
                         db,
                         &format!("reading:{}", kana.text),
+                        &current_id,
                         &frequency_map.get(&kana.text),
                         &kana.common,
                         "",
@@ -357,6 +364,7 @@ impl Dictionary {
     fn insert_entry(
         db: &Db,
         key: &str,
+        id: &str,
         frequency: &Option<&u32>,
         common: &bool,
         term: &str,
@@ -373,6 +381,7 @@ impl Dictionary {
             None => None,
         };
         let dictionary_term: DictionaryTerm = DictionaryTerm {
+            id: id.to_string(),
             frequency,
             common: *common,
             term: term.to_string(),
@@ -479,98 +488,6 @@ impl Dictionary {
 
         Ok(())
     }
-
-    /*
-    fn parse_jitendex(db: &Db) -> Result<(), Box<dyn Error>> {
-        let mut num = 1;
-        let mut current_path = format!(
-            "{}{}{}",
-            "./src/dictionaries/jitendex/term_bank_",
-            num.to_string(),
-            ".json"
-        );
-        while Path::new(&current_path).exists() {
-            let file: File = File::open(&current_path)?;
-            let entries: Vec<serde_json::Value> = serde_json::from_reader(BufReader::new(file))?;
-
-            for entry_value in &entries {
-                if let Some(entry_array) = entry_value.as_array() {
-                    if entry_array.len() >= 6 {
-                        // extract meanings
-                        let mut meanings: Vec<String> = Vec::new();
-                        if let Some(definitions) = entry_array[5].as_array() {
-                            if !definitions.is_empty() {
-                                meanings.extend(Self::jitendex_extract_meanings(&definitions[0]));
-                            }
-                        }
-                        if !meanings.is_empty() {
-                            let entry = DictionaryEntry {
-                                term: entry_array[0].as_str().unwrap_or("").to_string(),
-                                reading: entry_array[1].as_str().unwrap_or("").to_string(),
-                                meanings: meanings,
-                            };
-
-                            let serialized_entry =
-                                bincode::encode_to_vec(&entry, bincode::config::standard())?;
-
-                            if !entry.term.is_empty() {
-                                db.insert(
-                                    format!("term:{}", entry.term),
-                                    serialized_entry.as_slice(),
-                                )?;
-                            }
-
-                            if !entry.reading.is_empty() && entry.reading != entry.term {
-                                db.insert(
-                                    format!("reading:{}", entry.reading),
-                                    serialized_entry.as_slice(),
-                                )?;
-                            }
-                        }
-                    }
-                }
-            }
-
-            db.flush()?;
-            println!("Loaded {} entries into dictionary", entries.len());
-
-            num = num + 1;
-            current_path = format!(
-                "{}{}{}",
-                "./src/dictionaries/jitendex/term_bank_",
-                num.to_string(),
-                ".json"
-            );
-        }
-        Ok(())
-    }
-
-    fn jitendex_extract_meanings(value: &Value) -> Vec<String> {
-        let mut meanings = Vec::new();
-
-        match value {
-            Value::Object(map) => {
-                for (key, val) in map {
-                    if key == "content" {
-                        if let Value::String(s) = val {
-                            meanings.push(s.clone());
-                        } else {
-                            meanings.extend(Self::jitendex_extract_meanings(val));
-                        }
-                    } else {
-                        meanings.extend(Self::jitendex_extract_meanings(val));
-                    }
-                }
-            }
-            Value::Array(arr) => {
-                for item in arr {
-                    meanings.extend(Self::jitendex_extract_meanings(item));
-                }
-            }
-            _ => {}
-        }
-        meanings
-    }*/
 
     pub fn lookup(&self, word: &str) -> Result<Option<DictionaryEntry>, Box<dyn Error>> {
         if let Some(serialized_entry) = self.db.get(format!("term:{}", word))? {
