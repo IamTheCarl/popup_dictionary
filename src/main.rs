65230513f40e6916ef96943c3c6ad1613981d80c
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2025-11-24 18:28:26 +0100

    Implement copy argument
    Adds a copy argument which simulates ctrl+C presses and then gets the
    text from the clipboard. Should work on linux x11 and some wayland
    environments. Needs testing on macos. Doesn't seem to work on windows.
    Getting clipboard content is rather simple, but the ctrl+C keypresses
    are pretty inconsistent.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,59 @@
-use std::{env, process};
+use std::{
+    env,
+    process::{self, Command},
+};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
+
     let sentence: String = args
         .join("")
         .chars()
         .filter(|c| !c.is_whitespace())
         .collect();
-    if !sentence.is_empty() {
+
+    if sentence.starts_with("--copy") || sentence.starts_with("-c") {
+        use arboard::Clipboard;
+        use enigo::{Enigo, Keyboard};
+
+        // send Ctrl+C (twice as workaround for not always registering)
+        let mut enigo = Enigo::new(&enigo::Settings::default()).unwrap();
+        enigo.set_delay(100);
+        enigo
+            .key(enigo::Key::Control, enigo::Direction::Press)
+            .unwrap();
+        enigo
+            .key(enigo::Key::Unicode('c'), enigo::Direction::Click)
+            .unwrap();
+        std::thread::sleep(core::time::Duration::from_millis(100));
+        enigo
+            .key(enigo::Key::Control, enigo::Direction::Release)
+            .unwrap();
+        std::thread::sleep(core::time::Duration::from_millis(100));
+        enigo
+            .key(enigo::Key::Control, enigo::Direction::Press)
+            .unwrap();
+        enigo
+            .key(enigo::Key::Unicode('c'), enigo::Direction::Click)
+            .unwrap();
+        std::thread::sleep(core::time::Duration::from_millis(100));
+        enigo
+            .key(enigo::Key::Control, enigo::Direction::Release)
+            .unwrap();
+        std::thread::sleep(core::time::Duration::from_millis(100));
+
+        println!("creating clipboard");
+        if let Ok(mut clipboard) = Clipboard::new() {
+            println!("getting text");
+            if let Ok(sentence) = clipboard.get().text() {
+                println!("Clipboard content: {}", sentence);
+                if let Err(e) = popup_dictionary::run(&sentence) {
+                    eprintln!("Error: {e}");
+                    process::exit(1);
+                }
+            }
+        }
+    } else if !sentence.is_empty() {
         if let Err(e) = popup_dictionary::run(&sentence) {
             eprintln!("Error: {e}");
             process::exit(1);
