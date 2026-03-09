e4e41f305953045cf10bee48445cbb58bf656261
jasmine-blush <158479281+jasmine-blush@users.noreply.github.com>
2026-03-09 17:50:13 +0100

    Suppress tesseract console open on Windows

--- src/tesseract.rs
+++ src/tesseract.rs
@@ -1,3 +1,5 @@
+#[cfg(target_os = "windows")]
+use std::os::windows::process::CommandExt;
 use std::{
     error::Error,
     io::Write,
@@ -5,15 +7,24 @@ use std::{
 };
 
 const TESS_PATH_WINDOWS: &str = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe";
+const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 pub fn check_tesseract() -> Result<String, Box<dyn Error>> {
-    if let Err(e) = Command::new("tesseract").arg("--version").output() {
-        #[cfg(target_os = "linux")]
-        return Err(Box::new(e));
-        // tesseract installer on Windows doesn't set PATH automatically, check default dir without PATH
-        #[cfg(target_os = "windows")]
+    #[cfg(target_os = "linux")]
+    Command::new("tesseract").arg("--version").output()?;
+
+    // tesseract installer on Windows doesn't set PATH automatically, check default dir without PATH
+    #[cfg(target_os = "windows")]
+    {
+        if let Err(e) = Command::new("tesseract")
+            .arg("--version")
+            .creation_flags(CREATE_NO_WINDOW)
+            .output()
         {
-            Command::new(TESS_PATH_WINDOWS).arg("--version").output()?;
+            Command::new(TESS_PATH_WINDOWS)
+                .arg("--version")
+                .creation_flags(CREATE_NO_WINDOW)
+                .output()?;
             return Ok(String::from(
                 "C:\\Program Files\\Tesseract-OCR\\tesseract.exe",
             ));
@@ -25,6 +36,7 @@ pub fn check_tesseract() -> Result<String, Box<dyn Error>> {
 pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
     let tess_command: String = check_tesseract()?;
 
+    #[cfg(target_os = "linux")]
     let ver_conf_command = Command::new(&tess_command)
         .arg("stdin")
         .arg("stdout")
@@ -37,8 +49,23 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
+    #[cfg(target_os = "windows")]
+    let ver_conf_command = Command::new(&tess_command)
+        .arg("stdin")
+        .arg("stdout")
+        .arg("-l")
+        .arg("jpn_vert")
+        .arg("--psm")
+        .arg("5")
+        .arg("tsv")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .creation_flags(CREATE_NO_WINDOW)
+        .spawn()?;
     match get_conf(ver_conf_command, image_data) {
         Ok(ver_conf) => {
+            #[cfg(target_os = "linux")]
             let hor_conf_command = Command::new(&tess_command)
                 .arg("stdin")
                 .arg("stdout")
@@ -49,8 +76,21 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
                 .spawn()?;
+            #[cfg(target_os = "windows")]
+            let hor_conf_command = Command::new(&tess_command)
+                .arg("stdin")
+                .arg("stdout")
+                .arg("-l")
+                .arg("jpn")
+                .arg("tsv")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .creation_flags(CREATE_NO_WINDOW)
+                .spawn()?;
             let hor_conf: f32 = get_conf(hor_conf_command, image_data)?;
             if hor_conf >= ver_conf {
+                #[cfg(target_os = "linux")]
                 let hor_command = Command::new(&tess_command)
                     .arg("stdin")
                     .arg("stdout")
@@ -60,9 +100,21 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
                     .stdout(Stdio::piped())
                     .stderr(Stdio::piped())
                     .spawn()?;
+                #[cfg(target_os = "windows")]
+                let hor_command = Command::new(&tess_command)
+                    .arg("stdin")
+                    .arg("stdout")
+                    .arg("-l")
+                    .arg("jpn")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .creation_flags(CREATE_NO_WINDOW)
+                    .spawn()?;
                 let text = get_text(hor_command, image_data)?;
                 Ok(text)
             } else {
+                #[cfg(target_os = "linux")]
                 let ver_command = Command::new(&tess_command)
                     .arg("stdin")
                     .arg("stdout")
@@ -74,6 +126,19 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
                     .stdout(Stdio::piped())
                     .stderr(Stdio::piped())
                     .spawn()?;
+                #[cfg(target_os = "windows")]
+                let ver_command = Command::new(&tess_command)
+                    .arg("stdin")
+                    .arg("stdout")
+                    .arg("-l")
+                    .arg("jpn_vert")
+                    .arg("--psm")
+                    .arg("5")
+                    .stdin(Stdio::piped())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .creation_flags(CREATE_NO_WINDOW)
+                    .spawn()?;
                 let text = get_text(ver_command, image_data)?;
                 Ok(text)
             }
@@ -82,6 +147,7 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
             eprintln!(
                 "Couldn't parse vertical text. Make sure you have jpn_vert.traineddata installed if you want vertical text support."
             );
+            #[cfg(target_os = "linux")]
             let hor_command = Command::new(&tess_command)
                 .arg("stdin")
                 .arg("stdout")
@@ -90,6 +156,17 @@ pub fn ocr_image(image_data: &[u8]) -> Result<String, Box<dyn Error>> {
                 .stdin(Stdio::piped())
                 .stdout(Stdio::piped())
                 .stderr(Stdio::piped())
+                .spawn()?;
+            #[cfg(target_os = "windows")]
+            let hor_command = Command::new(&tess_command)
+                .arg("stdin")
+                .arg("stdout")
+                .arg("-l")
+                .arg("jpn")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .creation_flags(CREATE_NO_WINDOW)
                 .spawn()?;
             let text = get_text(hor_command, image_data)?;
             Ok(text)
